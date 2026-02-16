5f3240b445b94d042e1da9715c5d5d037205d7c6
gyaneshwar07 <154905180+gyaneshwar07@users.noreply.github.com>
2026-02-16 17:26:32 +0530

    Add solution: 74. Search a 2D Matrix

---
+++ 74. Search a 2D Matrix/74. Search a 2D Matrix.java
@@ -0,0 +1,22 @@
+/*
+ * Problem: 74. Search a 2D Matrix
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/search-a-2d-matrix/description/
+ * Language: java
+ * Date: 2026-02-16
+ */
+
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) { //i<,
+        int m = arr.length;
+        int n = arr[0].length;
+        int i = 0 , j = n-1;
+        while(i<m && j>=0){
+          if(arr[i][j]==target) return true;
+          else if (arr[i][j]>target) j--;
+          else i++;
+        }
+        return false;
+        
+    }
+}
