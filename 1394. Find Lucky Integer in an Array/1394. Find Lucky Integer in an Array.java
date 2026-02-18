fef40ebf46833883c1f858aa8a6a8ef052baa28d
gyaneshwar07 <154905180+gyaneshwar07@users.noreply.github.com>
2026-02-18 15:56:18 +0530

    Add solution: 1394. Find Lucky Integer in an Array

---
+++ 1394. Find Lucky Integer in an Array/1394. Find Lucky Integer in an Array.java
@@ -0,0 +1,26 @@
+/*
+ * Problem: 1394. Find Lucky Integer in an Array
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/find-lucky-integer-in-an-array/submissions/1923157894/
+ * Language: java
+ * Date: 2026-02-18
+ */
+
+class Solution {
+    public int findLucky(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        int mx = Integer.MIN_VALUE;
+        for(int key : map.keySet()){
+            int val = map.get(key);
+            if(val==key){
+                if(key>mx) mx=key;
+            }
+        }
+        if(mx==Integer.MIN_VALUE) return -1;
+        return mx;
+    }
+}
