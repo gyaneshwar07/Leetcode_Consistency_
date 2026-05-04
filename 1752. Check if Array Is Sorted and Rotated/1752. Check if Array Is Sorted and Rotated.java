0343ebbadf8f445497435ca790ed4f6b2e94ab7d
gyaneshwar07 <154905180+gyaneshwar07@users.noreply.github.com>
2026-05-04 15:08:47 +0530

    Update solution: 1752. Check if Array Is Sorted and Rotated

--- 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.java
+++ 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.java
@@ -7,44 +7,14 @@
  */
 
 class Solution {
-    public boolean Sorted(int[] nums){
-        int n = nums.length;
-        boolean flag = true;
-        for(int i=1;i<n;i++){
-           if(nums[i]<nums[i-1]) flag = false;
-        }
-        if(flag==true) return true;
-        flag = true;
-        for(int i=n-2;i>=0;i--){
-           if(nums[i]<nums[i+1]) flag = false;
-        }
-        if(flag==true) return true;
-        return false;
-    }
-    public void reverse(int[] nums , int i , int j){
-        while(i<=j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;j--;
-        }
-    }
     public boolean check(int[] nums) {
         int n = nums.length;
-        int j = 0;
-        boolean flag = true;
+        int count = 0;
         for(int i=1;i<n;i++){
-            if(nums[i]<nums[i-1]){
-               j=i;
-               flag = false;
-               break;
-            }
+           if(nums[i]<nums[i-1]) count++;
         }
-        if(flag) return true;
-        reverse(nums,0,j-1);
-        reverse(nums,j,n-1);
-        reverse(nums,0,n-1);
-
-        return Sorted(nums);
+        if(nums[0]<nums[n-1]) count++;
+        if(count<=1) return true;
+        return false;
     }
 }
