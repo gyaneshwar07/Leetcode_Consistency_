025b6855843cddc47554f113db66ac1be0d62318
gyaneshwar07 <154905180+gyaneshwar07@users.noreply.github.com>
2026-05-05 15:45:17 +0530

    Update solution: 704. Binary Search

--- 704. Binary Search/704. Binary Search.java
+++ 704. Binary Search/704. Binary Search.java
@@ -1,21 +1,24 @@
 /*
  * Problem: 704. Binary Search
  * Difficulty: Easy
- * Link: https://leetcode.com/problems/binary-search/description/
+ * Link: https://leetcode.com/problems/binary-search/
  * Language: java
  * Date: 2026-05-05
  */
 
 class Solution {
+    public int Solve(int[] nums , int lo , int hi , int k){
+        if(lo>hi){
+            return -1;
+        }
+        int mid = lo + (hi-lo)/2;
+        if(nums[mid]==k) return mid;
+        else if(nums[mid]>k)  return Solve(nums,lo,mid-1,k);
+        else return Solve(nums,mid+1,hi,k);
+        
+    }
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int lo = 0 , hi = n-1;
-        while(lo<=hi){
-            int mid = lo + (hi-lo)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) hi=mid-1;
-            else lo = mid+1;
-        }
-        return -1;
+        return Solve(nums,0,n-1,target);
     }
 }
