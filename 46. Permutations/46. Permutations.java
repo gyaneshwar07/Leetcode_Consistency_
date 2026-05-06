6f02a873ac452e1a502f3705b2a748ef06d589af
gyaneshwar07 <154905180+gyaneshwar07@users.noreply.github.com>
2026-05-06 17:31:25 +0530

    Add solution: 46. Permutations

---
+++ 46. Permutations/46. Permutations.java
@@ -0,0 +1,33 @@
+/*
+ * Problem: 46. Permutations
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/permutations/
+ * Language: java
+ * Date: 2026-05-06
+ */
+
+class Solution {
+    public void helper(int[] nums,List<Integer> ds,boolean[] isValid,List<List<Integer>> ans){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(isValid[i]==false){
+                ds.add(nums[i]);
+                isValid[i] = true;
+                helper(nums,ds,isValid,ans);
+                isValid[i] = false;
+                ds.remove(ds.size()-1);
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] isValid = new boolean[nums.length];
+        helper(nums,ds,isValid,ans);
+        return ans;
+    }
+}
